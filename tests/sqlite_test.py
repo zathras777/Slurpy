b9d63f282466ad92eabd35ad7fb9d9df9b05d6a6
david reid <zathrasorama@gmail.com>
2012-02-08 17:33:15 +0000

    Changes to fix an error thrown up on OSX

--- tests/sqlite_test.py
+++ tests/sqlite_test.py
@@ -4,18 +4,23 @@ import unittest
 
 from slurpy.databases.sqlite import SqliteDatabase
 
+testDir = os.path.dirname(__file__)
+
 class TestDB(unittest.TestCase):
+    def setUp(self):
+        self.db = SqliteDatabase()
+        
     def test_001(self):
-        db = SqliteDatabase()
-        self.assertNotEqual(db, None)
-        self.assertRaises(ValueError, db.connect)
-        os.unlink('tests.db')
-        self.assertEqual(db.connect(dbname = 'tests.db'), True)
-        self.assertEqual(db.close(), True)
-        self.assertEqual(db.close(), True)
+        self.assertNotEqual(self.db, None)
+        self.assertRaises(ValueError, self.db.connect)
+        fn = os.path.join(testDir, 'tests.db')
+        if os.path.exists(fn):
+            os.unlink(fn)
+        self.assertEqual(self.db.connect(dbname = fn), True)
+        self.assertEqual(self.db.close(), True)
+        self.assertEqual(self.db.close(), True)
         
     def test_002_simple(self):
-        db = SqliteDatabase()
-        self.assertNotEqual(db, None)
-        self.assertEqual(db.connect(dbname = ':memory:'), True)
+        self.assertNotEqual(self.db, None)
+        self.assertEqual(self.db.connect(dbname = ':memory:'), True)
                 
